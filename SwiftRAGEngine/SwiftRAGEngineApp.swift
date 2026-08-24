4f0a007de86003522ee7ad8347f9848a0b437daf
Andrew Wallace <japawallace16@gmail.com>
2026-08-24 10:32:54 +0700

    chore: init

---
+++ SwiftRAGEngine/SwiftRAGEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRAGEngineApp.swift
+//  SwiftRAGEngine
+//
+//  Created by Andrew Wallace on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRAGEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
